11a0e6875e77cb4ccf33842686f165f6cd500226
corinnekrych <corinnekrych@gmail.com>
2013-10-17 22:45:19 +0200

    without test

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk && xctool -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdkTests
+xctool -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk 
 
