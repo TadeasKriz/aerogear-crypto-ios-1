db64a1635ccf9f58cf5f640e74ec6ee1063891c3
Tadeas Kriz <tadeas.kriz@gmail.com>
2013-10-18 13:47:09 +0200

    Another.

--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-xcodebuild -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk -sdk iphonesimulator -configuration Debug clean test 
-#VALID_ARCHS="armv7 armv7s i386" ARCHS="armv7 armv7s i386" ONLY_ACTIVE_ARCH=NO
+xcodebuild -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk -configuration Debug clean test VALID_ARCHS="armv7 armv7s i386" ARCHS="armv7 armv7s i386" ONLY_ACTIVE_ARCH=YES
+#
