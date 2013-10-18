d857e98ae64c161e5fca7c0dbc0aea94f0b64db8
Tadeas Kriz <tadeas.kriz@gmail.com>
2013-10-18 14:55:11 +0200

    Another.

--- travis/script.sh
+++ travis/script.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 
-- ios-sim start --verbose &
+ios-sim start --verbose &
 
-- xcodebuild -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk -sdk iphonesimulator -configuration Debug -showBuildSettings VALID_ARCHS="armv7 armv7s i386"
+#xcodebuild -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk -sdk iphonesimulator -configuration Debug -showBuildSettings VALID_ARCHS="armv7 armv7s i386"
 
-- xcodebuild -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk -sdk iphonesimulator -configuration Debug clean build test VALID_ARCHS="armv7 armv7s i386" ONLY_ACTIVE_ARCH=NO
-#
+xcodebuild -workspace crypto-sdk.xcworkspace/ -scheme crypto-sdk -sdk iphonesimulator -configuration Debug clean build test VALID_ARCHS="armv7 armv7s i386" ONLY_ACTIVE_ARCH=NO
